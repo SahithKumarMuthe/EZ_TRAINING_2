1fe809e33f21e8c7fa33c43359219736b369d3da
SahithKumarMuthe <146471502+SahithKumarMuthe@users.noreply.github.com>
2023-10-07 11:20:20 +0530

    Update binarysort.py

--- Day 9/binarysort.py
+++ Day 9/binarysort.py
@@ -2,23 +2,18 @@ def binary_search(arr, target):
     left, right = 0, len(arr) - 1
 
     while left <= right:
-        mid = left + (right - left) // 2  # Calculate the middle index
+        mid = left + (right - left) // 2 
 
         if arr[mid] == target:
-            return mid  # Target element found, return its index
+            return mid 
         elif arr[mid] < target:
-            left = mid + 1  # Target is in the right half
+            left = mid + 1  
         else:
-            right = mid - 1  # Target is in the left half
-
-    return -1  # Target element not found in the array
-
-# Example usage:
+            right = mid - 1 
+    return -1 
 sorted_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 target_element = 7
-
 result = binary_search(sorted_list, target_element)
-
 if result != -1:
     print(f"{target_element} found at index {result}")
 else:
